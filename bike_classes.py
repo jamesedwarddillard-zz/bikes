fede7d4689a82b6f257f6de36cf8f9d22baa4f0b
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-05 19:57:16 -0400

    started creating a wheels class

---
+++ bike_classes.py
@@ -0,0 +1,12 @@
+"""Creating a set of classes to model the bicycle industry, including:
+Wheels
+Frames
+Models
+Manufacturers
+Shops
+Customers"""
+
+#defining the wheels class
+class Wheels(object):
+	def __init__(self):
+		pass
