ffbc0271c42af22f9cbfcb70c865d210932487d0
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-06 11:28:11 -0400

    added a bike maker class, adjusted model class and created wholesale function

--- bike_classes.py
+++ bike_classes.py
@@ -22,15 +22,37 @@ class Frames(object):
 		self.cost = cost
 		self.weight = weight
 
-#defining bicycle models
+#defining bicycle models class
 class Models(object):
-	def __init__(self, name, wheels, frame, manufactuer):
+	maker = str()
+	wholesale = int()
+	def __init__(self, name, wheels, frame):
 		self.name = name
 		self.wheels = wheels
 		self.frame = frame
-		self.manufactuer = manufactuer
 		self.weight = (wheels.weight *2) + frame.weight
 		self.cost = (wheels.cost *2) + frame.cost
+
+#creating a wholesale price function
+def wholesale(manufactured_cost, margin):
+	return manufactured_cost * (1+margin)
+
+
+#defining bicycle manufacturers class
+class Maker(object):
+	def __init__(self, name, profit_margin, catalog):
+		self.name = name
+		self.profit_margin = profit_margin
+		self.catalog = catalog
+		for model in catalog: 
+			model.wholesale = wholesale(model.cost, profit_margin)
+			model.maker = self.name
+
+
+
+
+
+
 
 """instances go here"""
 
@@ -43,3 +65,14 @@ bigwheel = Wheels('bigwheels', 150)
 aluminum = Frames('aluminum', 50, 1.0)
 carbon = Frames('carbon', 125, 0.5)
 steel = Frames('steel', 40, 2.5)
+
+# scratch for testing
+
+big_bike = Models('bigassbike', bigwheel, steel)
+light_bike = Models('airbike', smallwheel, carbon)
+bike_list = [big_bike, light_bike]
+
+james_bikes = Maker("James's Bike Co.", 0.40, bike_list)
+
+for i in james_bikes.catalog:
+	print i.name, i.wholesale, i.maker
