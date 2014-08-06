fa44df86aa6b3861e080df8b2d083a2466c26a6e
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-06 13:30:33 -0400

    created three types of wheels in my script

--- bike_classes.py
+++ bike_classes.py
@@ -80,5 +80,3 @@ class Customer(object):
 	#bike buying function
 
 
-
-print 'WORKED'
--- bike_script.py
+++ bike_script.py
@@ -1,1 +1,8 @@
-import bike_classes
+from bike_classes import *
+
+#creating the world of wheels available
+small_wheel = Wheels('small', 10)
+medium_wheel = Wheels('medium', 15)
+large_wheel = Wheels('large_wheel', 17)
+
+#creating the world of frames available
