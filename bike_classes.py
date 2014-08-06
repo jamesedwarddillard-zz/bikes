0ef73d2cee7c2309fe16b04cafd03e62743ed4bb
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-06 08:35:47 -0400

    separated classes from instances

--- bike_classes.py
+++ bike_classes.py
@@ -6,17 +6,14 @@ Manufacturers
 Shops
 Customers"""
 
+"""classes go here"""
+
 #defining the wheels class
 class Wheels(object):
 	def __init__(self, model, cost):
 		self.model = model
 		self.cost = cost
 		self.weight = 1.0 #weight in pounds for all wheels		
-
-# creating three wheel instances
-smallwheel = Wheels('smallwheels', 50)
-mediumwheel = Wheels('mediumwheels', 100)
-bigwheel = Wheels('bigwheels', 150)
 
 #defining the frames class 
 class Frames(object):
@@ -25,8 +22,16 @@ class Frames(object):
 		self.cost = cost
 		self.weight = weight
 
+
+
+"""instances go here"""
+
+# creating three wheel instances
+smallwheel = Wheels('smallwheels', 50)
+mediumwheel = Wheels('mediumwheels', 100)
+bigwheel = Wheels('bigwheels', 150)
+
 #creating three frame instances
 aluminum = Frames('aluminum', 50, 1.0)
 carbon = Frames('carbon', 125, 0.5)
 steel = Frames('steel', 40, 2.5)
-
