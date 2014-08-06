cb32d68adcb255517d6fc0120c863ac0570ba505
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-06 13:33:55 -0400

    created three types of frames

--- bike_script.py
+++ bike_script.py
@@ -6,3 +6,6 @@ medium_wheel = Wheels('medium', 15)
 large_wheel = Wheels('large_wheel', 17)
 
 #creating the world of frames available
+aluminum = Frames('aluminum', 20, 0.5)
+steel = Frames('steel', 7, 3.0)
+carbon = Frames('carbon', 30, 0.7)
